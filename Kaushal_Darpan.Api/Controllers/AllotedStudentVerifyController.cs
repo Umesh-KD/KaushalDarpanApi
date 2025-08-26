965653deb00c7a01fc241630fee1d87d58afb658
Brijraj Sharma <brijraj.sharma@corpnet.co.in>
2025-08-26 17:36:27 +0530

    allotment to addimitted and selected to enrollment. for acp approve and return

---
+++ Kaushal_Darpan.Api/Controllers/AllotedStudentVerifyController.cs
@@ -0,0 +1,186 @@
+﻿using AutoMapper;
+using Kaushal_Darpan.Api.Code.Attribute;
+using Kaushal_Darpan.Core.Helper;
+using Kaushal_Darpan.Core.Interfaces;
+using Kaushal_Darpan.Infra.Repositories;
+using Kaushal_Darpan.Models.ApplicationData;
+using Kaushal_Darpan.Models.PlacementSelectedStudentMaster;
+using Kaushal_Darpan.Models.PlacementShortListStudentMaster;
+using Kaushal_Darpan.Models.PreExamStudent;
+using Kaushal_Darpan.Models.Student;
+using Kaushal_Darpan.Models.StudentMaster;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+
+namespace Kaushal_Darpan.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //[CustomeAuthorize]
+    //[CustomeAuthorize(EnumRole.Admin,EnumRole.Guest)]
+    [ValidationActionFilter]
+    public class AllotedStudentVerifyController : BaseController
+    {
+        public override string PageName => "AllotedStudentVerifyController";
+        public override string ActionName { get; set; }
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AllotedStudentVerifyController(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        [RoleActionFilter(EnumRole.ACP, EnumRole.ACP_NonEng)]
+        [HttpPost("GetAdmittedStudentToVerify")]
+        public async Task<ApiResult<DataTable>> GetAdmittedStudentToVerify(StudentApplicationModel model)
+        {
+            ActionName = "GetStudentAdmitted(StudentApplicationModel model)";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+                result.Data = await _unitOfWork.StudentEnrollmentRepository.GetAdmittedStudentToVerify(model);
+                result.State = EnumStatus.Success;
+                if (result.Data.Rows.Count == 0)
+                {
+                    result.Message = Constants.MSG_DATA_NOT_FOUND;
+                }
+                else
+                {
+                    result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                _unitOfWork.Dispose();
+                result.State = EnumStatus.Error;
+                result.Message = Constants.MSG_ERROR_OCCURRED;
+                result.ErrorMessage = ex.Message;
+                // write error log
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
+
+        [RoleActionFilter(EnumRole.ACP, EnumRole.ACP_NonEng)]
+        [HttpPost("SaveAdmittedStudentForApproveByAcp")]
+        public async Task<ApiResult<bool>> SaveAdmittedStudentForApproveByAcp([FromBody] List<StudentApplicationSaveModel> request)
+        {
+            ActionName = "SaveAdmittedStudentForApproveByAcp([FromBody] List<StudentApplicationSaveModel> request)";
+            return await Task.Run(async () =>
+            {
+                var result = new ApiResult<bool>();
+                try
+                {
+                    //ipaddress
+                    request.ForEach(x =>
+                    {
+                        x.IPAddress = CommonFuncationHelper.GetIpAddress();
+                    });
+                    // Pass the list to the repository for batch update
+                    var isSave = await _unitOfWork.StudentEnrollmentRepository.SaveAdmittedStudentForApproveByAcp(request);
+                    _unitOfWork.SaveChanges();  // Commit changes if everything is successful
+
+                    if (isSave == -1)
+                    {
+                        result.Data = true;
+                        result.State = EnumStatus.Warning;
+                        result.Message = Constants.MSG_NO_DATA_SAVE;
+                    }
+                    else if (isSave > 0)
+                    {
+                        result.Data = true;
+                        result.State = EnumStatus.Success;
+                        result.Message = Constants.MSG_SAVE_SUCCESS;
+                    }
+                    else
+                    {
+                        result.State = EnumStatus.Error;
+                        result.ErrorMessage = Constants.MSG_ADD_ERROR;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _unitOfWork.Dispose();
+                    result.State = EnumStatus.Error;
+                    result.ErrorMessage = ex.Message;
+
+                    // Log the error
+                    var nex = new NewException
+                    {
+                        PageName = PageName,
+                        ActionName = ActionName,
+                        Ex = ex,
+                    };
+                    await CreateErrorLog(nex, _unitOfWork);
+                }
+                return result;
+            });
+        }
+        
+        [RoleActionFilter(EnumRole.ACP, EnumRole.ACP_NonEng)]
+        [HttpPost("SaveAdmittedStudentForReturnByAcp")]
+        public async Task<ApiResult<bool>> SaveAdmittedStudentForReturnByAcp([FromBody] List<StudentApplicationSaveModel> request)
+        {
+            ActionName = "SaveAdmittedStudentForReturnByAcp([FromBody] List<StudentApplicationSaveModel> request)";
+            return await Task.Run(async () =>
+            {
+                var result = new ApiResult<bool>();
+                try
+                {
+                    //ipaddress
+                    request.ForEach(x =>
+                    {
+                        x.IPAddress = CommonFuncationHelper.GetIpAddress();
+                    });
+                    // Pass the list to the repository for batch update
+                    var isSave = await _unitOfWork.StudentEnrollmentRepository.SaveAdmittedStudentForReturnByAcp(request);
+                    _unitOfWork.SaveChanges();  // Commit changes if everything is successful
+
+                    if (isSave == -1)
+                    {
+                        result.Data = true;
+                        result.State = EnumStatus.Warning;
+                        result.Message = Constants.MSG_NO_DATA_SAVE;
+                    }
+                    else if (isSave > 0)
+                    {
+                        result.Data = true;
+                        result.State = EnumStatus.Success;
+                        result.Message = Constants.MSG_SAVE_SUCCESS;
+                    }
+                    else
+                    {
+                        result.State = EnumStatus.Error;
+                        result.ErrorMessage = Constants.MSG_ADD_ERROR;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _unitOfWork.Dispose();
+                    result.State = EnumStatus.Error;
+                    result.ErrorMessage = ex.Message;
+
+                    // Log the error
+                    var nex = new NewException
+                    {
+                        PageName = PageName,
+                        ActionName = ActionName,
+                        Ex = ex,
+                    };
+                    await CreateErrorLog(nex, _unitOfWork);
+                }
+                return result;
+            });
+        }
+    }
+}
