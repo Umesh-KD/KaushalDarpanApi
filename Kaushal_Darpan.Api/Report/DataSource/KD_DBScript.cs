15aebaef3d7718e6409c413cc2a7bd97f199527d
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2025-08-28 16:19:21 +0530

    kd script student fee receipt

--- Kaushal_Darpan.Api/Report/DataSource/KD_DBScript.cs
+++ Kaushal_Darpan.Api/Report/DataSource/KD_DBScript.cs
@@ -2709,3 +2709,11 @@ namespace Kaushal_Darpan.Api.Report.DataSource
 namespace Kaushal_Darpan.Api.Report.DataSource
 {
 }
+
+namespace Kaushal_Darpan.Api.Report.DataSource
+{
+}
+
+namespace Kaushal_Darpan.Api.Report.DataSource
+{
+}
