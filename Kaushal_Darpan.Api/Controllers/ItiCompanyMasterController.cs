66735023242638909dc5ce475d806fe84d5accdd
Jafar Alam <jafar.alam@corpnet.co.in>
2025-09-12 15:03:18 +0530

    iti companymaster placement added hr fields

--- Kaushal_Darpan.Api/Controllers/ItiCompanyMasterController.cs
+++ Kaushal_Darpan.Api/Controllers/ItiCompanyMasterController.cs
@@ -2,6 +2,7 @@
 using Kaushal_Darpan.Api.Code.Attribute;
 using Kaushal_Darpan.Core.Helper;
 using Kaushal_Darpan.Core.Interfaces;
+using Kaushal_Darpan.Models.CompanyMaster;
 using Kaushal_Darpan.Models.ItiCompanyMaster;
 
 //using Kaushal_Darpan.Models.CompanyMaster;
@@ -306,6 +307,47 @@ namespace Kaushal_Darpan.Api.Controllers
             }
             return result;
         }
+
+
+        [HttpPost("CompanyMasterReport")]
+        public async Task<ApiResult<DataTable>> CompanyMasterReport([FromBody] ItiCompanyMasterSearchModel body)
+        {
+            ActionName = "CompanyMasterReport([FromBody] CompanyMasterSearchModel body)";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+                result.Data = await Task.Run(() => _unitOfWork.i_ITICompanyMasterRepository.CompanyMasterReport(body));
+                result.State = EnumStatus.Success;
+                if (result.Data.Rows.Count == 0)
+                {
+                    result.State = EnumStatus.Success;
+                    result.Message = "No record found.!";
+                    return result;
+                }
+                result.State = EnumStatus.Success;
+                result.Message = "Data load successfully .!";
+            }
+            catch (System.Exception ex)
+            {
+                _unitOfWork.Dispose();
+                result.State = EnumStatus.Error;
+                result.ErrorMessage = ex.Message;
+                // write error log
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
+
+
+
+
+
     }
 
 
