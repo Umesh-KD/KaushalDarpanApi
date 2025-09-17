9a507e413a333d4a053ef14f1a55e685f3a947f6
Jafar Alam <jafar.alam@corpnet.co.in>
2025-09-17 13:12:09 +0530

    eligible placement student

--- Kaushal_Darpan.Models/CompanyMaster/CompanyMasterSearchModel.cs
+++ Kaushal_Darpan.Models/CompanyMaster/CompanyMasterSearchModel.cs
@@ -8,4 +8,14 @@
         public int RoleID { get; set; }
         public int DepartmentID { get; set; }
     }
+
+    public class EligibleStudentListMasterSearchModel
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public int ModifyBy { get; set; }
+        public int RoleID { get; set; }
+        public int DepartmentID { get; set; }
+        public int InstituteID { get; set; }
+    }
 }
