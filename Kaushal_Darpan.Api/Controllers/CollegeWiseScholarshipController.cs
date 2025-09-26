38cf40e88962086b8c98c25a70ac8f9ab8f4b035
Jafar Alam <jafar.alam@corpnet.co.in>
2025-09-26 16:29:54 +0530

    changes to be mas

---
+++ Kaushal_Darpan.Api/Controllers/CollegeWiseScholarshipController.cs
@@ -0,0 +1,442 @@
+﻿using AutoMapper;
+using Kaushal_Darpan.Api.Code.Attribute;
+using Kaushal_Darpan.Core.Helper;
+using Kaushal_Darpan.Core.Interfaces;
+using Kaushal_Darpan.Models.CollegeWiseScholarship;
+using Kaushal_Darpan.Models.CompanyMaster;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Kaushal_Darpan.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //[CustomeAuthorize]
+    [ValidationActionFilter]
+    public class CollegeWiseScholarshipController : BaseController
+    {
+        public override string PageName => "CollegeWiseScholarshipController";
+        public override string ActionName { get; set; }
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CollegeWiseScholarshipController(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        //[HttpPost("GetAllData")]
+        //public async Task<ApiResult<DataTable>> GetAllData([FromBody] CompanyMasterSearchModel body)
+        //{
+        //    ActionName = "GetAllData()";
+        //    var result = new ApiResult<DataTable>();
+        //    try
+        //    {
+
+        //        // Pass the entire model to the repository
+        //        result.Data = await _unitOfWork.CompanyMasterRepository.GetAllData(body);
+
+        //        if (result.Data.Rows.Count > 0)
+        //        {
+        //            result.State = EnumStatus.Success;
+        //            result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+        //        }
+        //        else
+        //        {
+        //            result.State = EnumStatus.Warning;
+        //            result.Message = Constants.MSG_DATA_NOT_FOUND;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        result.State = EnumStatus.Error;
+        //        result.ErrorMessage = ex.Message;
+
+        //        // Log the error
+        //        await _unitOfWork.DisposeAsync();
+        //        var nex = new NewException
+        //        {
+        //            PageName = PageName,
+        //            ActionName = ActionName,
+        //            Ex = ex,
+        //        };
+        //        await CreateErrorLog(nex, _unitOfWork);
+        //    }
+        //    return result;
+        //}
+
+
+        //[HttpGet("GetByID/{PK_ID}")]
+        //public async Task<ApiResult<CompanyMasterResponsiveModel>> GetByID(int PK_ID)
+        //{
+        //    ActionName = "GetByID(int PK_ID)";
+        //    return await Task.Run(async () =>
+        //    {
+        //        var result = new ApiResult<CompanyMasterResponsiveModel>();
+        //        try
+        //        {
+        //            var data = await _unitOfWork.CompanyMasterRepository.GetById(PK_ID);
+        //            result.Data = data;
+        //            if (data != null)
+        //            {
+        //                result.State = EnumStatus.Success;
+        //                result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+        //            }
+        //            else
+        //            {
+        //                result.State = EnumStatus.Warning;
+        //                result.Message = Constants.MSG_DATA_NOT_FOUND;
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            await _unitOfWork.DisposeAsync();
+        //            // Write error log
+        //            var nex = new NewException
+        //            {
+        //                PageName = PageName,
+        //                ActionName = ActionName,
+        //                Ex = ex,
+        //            };
+        //            await CreateErrorLog(nex, _unitOfWork);
+        //            result.State = EnumStatus.Error;
+        //            result.ErrorMessage = ex.Message;
+        //        }
+        //        return result;
+        //    });
+        //}
+
+        //[HttpPost("SaveData")]
+        //public async Task<ApiResult<bool>> SaveData([FromBody] CompanyMasterModels request)
+        //{
+        //    ActionName = "SaveData([FromBody] CompanyMasterModels request)";
+        //    return await Task.Run(async () =>
+        //    {
+        //        var result = new ApiResult<bool>();
+        //        try
+        //        {
+
+        //            if (!ModelState.IsValid)
+        //            {
+        //                result.State = EnumStatus.Error;
+        //                result.ErrorMessage = "Validation failed!";
+        //                return result;
+        //            }
+
+
+        //            result.Data = await _unitOfWork.CompanyMasterRepository.SaveData(request);
+        //            await _unitOfWork.SaveChangesAsync();
+        //            if (result.Data)
+        //            {
+        //                result.State = EnumStatus.Success;
+        //                if (request.ID == 0)
+        //                {
+        //                    result.Message = Constants.MSG_SAVE_SUCCESS;
+        //                }
+        //                else
+        //                {
+        //                    result.Message = Constants.MSG_UPDATE_SUCCESS;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                result.State = EnumStatus.Error;
+        //                if (request.ID == 0)
+        //                {
+        //                    result.ErrorMessage = Constants.MSG_ADD_ERROR;
+        //                }
+        //                else
+        //                {
+        //                    result.ErrorMessage = Constants.MSG_UPDATE_ERROR;
+        //                }
+        //            }
+        //        }
+        //        catch (System.Exception ex)
+        //        {
+        //            await _unitOfWork.DisposeAsync();
+        //            result.State = EnumStatus.Error;
+        //            result.ErrorMessage = ex.Message;
+        //            // write error log
+        //            var nex = new NewException
+        //            {
+        //                PageName = PageName,
+        //                ActionName = ActionName,
+        //                Ex = ex,
+        //            };
+        //            await CreateErrorLog(nex, _unitOfWork);
+        //        }
+        //        return result;
+        //    });
+        //}
+
+        //[HttpPost("DeleteByID/{ID:int}/{ModifyBy:int}")]
+      
+        //public async Task<ApiResult<bool>> DeleteByID(int ID, int ModifyBy)
+        //{
+        //    ActionName = "DeleteByID(int ID, int ModifyBy)";
+        //    return await Task.Run(async () =>
+        //    {
+        //        var result = new ApiResult<bool>();
+        //        try
+        //        {
+        //            var mappedData = new CompanyMasterModels
+        //            {
+        //                ID = ID,
+        //                ModifyBy = ModifyBy,
+
+        //                ActiveStatus = false,
+        //                DeleteStatus = true,
+        //            };
+        //            result.Data = await _unitOfWork.CompanyMasterRepository.DeleteDataByID(mappedData);
+        //            await _unitOfWork.SaveChangesAsync();
+
+        //            if (result.Data)
+        //            {
+        //                result.State = EnumStatus.Success;
+        //                result.Message = Constants.MSG_DELETE_SUCCESS;
+        //            }
+        //            else
+        //            {
+        //                result.State = EnumStatus.Error;
+        //                result.ErrorMessage = Constants.MSG_DELETE_ERROR;
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            await _unitOfWork.DisposeAsync();
+        //            // Write error log
+        //            var nex = new NewException
+        //            {
+        //                PageName = PageName,
+        //                ActionName = ActionName,
+        //                Ex = ex,
+        //            };
+        //            await CreateErrorLog(nex, _unitOfWork);
+        //            result.State = EnumStatus.Error;
+        //            result.ErrorMessage = ex.Message;
+        //        }
+        //        return result;
+        //    });
+        //}
+
+
+        //[HttpPost("Save_CompanyValidation_NodalAction")]
+        //public async Task<ApiResult<bool>> Save_CompanyValidation_NodalAction([FromBody] CompanyMaster_Action request)
+        //{
+        //    ActionName = "Save_CompanyValidation_NodalAction([FromBody] CompanyMaster_Action request)";
+        //    return await Task.Run(async () =>
+        //    {
+        //        var result = new ApiResult<bool>();
+        //        try
+        //        {
+
+        //            if (!ModelState.IsValid)
+        //            {
+        //                result.State = EnumStatus.Error;
+        //                result.ErrorMessage = "Validation failed!";
+        //                return result;
+        //            }
+
+
+        //            result.Data = await _unitOfWork.CompanyMasterRepository.Save_CompanyValidation_NodalAction(request);
+        //            await _unitOfWork.SaveChangesAsync();
+        //            if (result.Data)
+        //            {
+        //                result.State = EnumStatus.Success;
+        //                result.Message = "Updated successfully .!";
+        //            }
+        //            else
+        //            {
+        //                result.State = EnumStatus.Error;
+
+        //                result.ErrorMessage = "There was an error updating data.!";
+        //            }
+        //        }
+        //        catch (System.Exception ex)
+        //        {
+        //            await _unitOfWork.DisposeAsync();
+        //            result.State = EnumStatus.Error;
+        //            result.ErrorMessage = ex.Message;
+        //            // write error log
+        //            var nex = new NewException
+        //            {
+        //                PageName = PageName,
+        //                ActionName = ActionName,
+        //                Ex = ex,
+        //            };
+        //            await CreateErrorLog(nex, _unitOfWork);
+        //        }
+        //        return result;
+        //    });
+        //}
+
+        //[HttpPost("CompanyValidationList")]
+        //public async Task<ApiResult<DataTable>> CompanyValidationList([FromBody] CompanyMasterSearchModel body)
+        //{
+        //    ActionName = "GetAllData()";
+        //    var result = new ApiResult<DataTable>();
+        //    try
+        //    {
+        //        result.Data = await Task.Run(() => _unitOfWork.CompanyMasterRepository.CompanyValidationList(body));
+        //        result.State = EnumStatus.Success;
+        //        if (result.Data.Rows.Count == 0)
+        //        {
+        //            result.State = EnumStatus.Success;
+        //            result.Message = "No record found.!";
+        //            return result;
+        //        }
+        //        result.State = EnumStatus.Success;
+        //        result.Message = "Data load successfully .!";
+        //    }
+        //    catch (System.Exception ex)
+        //    {
+        //        await _unitOfWork.DisposeAsync();
+        //        result.State = EnumStatus.Error;
+        //        result.ErrorMessage = ex.Message;
+        //        // write error log
+        //        var nex = new NewException
+        //        {
+        //            PageName = PageName,
+        //            ActionName = ActionName,
+        //            Ex = ex,
+        //        };
+        //        await CreateErrorLog(nex, _unitOfWork);
+        //    }
+        //    return result;
+        //}
+
+
+
+        //[HttpPost("CompanyMasterReport")]
+        //public async Task<ApiResult<DataTable>> CompanyMasterReport([FromBody] CompanyMasterSearchModel body)
+        //{
+        //    ActionName = "CompanyMasterReport([FromBody] CompanyMasterSearchModel body)";
+        //    var result = new ApiResult<DataTable>();
+        //    try
+        //    {
+        //        result.Data = await Task.Run(() => _unitOfWork.CompanyMasterRepository.CompanyMasterReport(body));
+        //        result.State = EnumStatus.Success;
+        //        if (result.Data.Rows.Count == 0)
+        //        {
+        //            result.State = EnumStatus.Success;
+        //            result.Message = "No record found.!";
+        //            return result;
+        //        }
+        //        result.State = EnumStatus.Success;
+        //        result.Message = "Data load successfully .!";
+        //    }
+        //    catch (System.Exception ex)
+        //    {
+        //        await _unitOfWork.DisposeAsync();
+        //        result.State = EnumStatus.Error;
+        //        result.ErrorMessage = ex.Message;
+        //        // write error log
+        //        var nex = new NewException
+        //        {
+        //            PageName = PageName,
+        //            ActionName = ActionName,
+        //            Ex = ex,
+        //        };
+        //        await CreateErrorLog(nex, _unitOfWork);
+        //    }
+        //    return result;
+        //}
+
+
+
+        [HttpPost("GetCollegeWiseScholarshipList")]
+        public async Task<ApiResult<DataTable>> GetCollegeWiseScholarshipList([FromBody] CollegeWiseScholarshipSearchModel body)
+        {
+            ActionName = "GetCollegeWiseScholarshipList()";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+
+                // Pass the entire model to the repository
+                result.Data = await _unitOfWork.CollegeWiseScholarshipRepository.GetCollegeWiseScholarshipList(body);
+
+                if (result.Data.Rows.Count > 0)
+                {
+                    result.State = EnumStatus.Success;
+                    result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+                }
+                else
+                {
+                    result.State = EnumStatus.Warning;
+                    result.Message = Constants.MSG_DATA_NOT_FOUND;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.State = EnumStatus.Error;
+                result.ErrorMessage = ex.Message;
+
+                // Log the error
+                await _unitOfWork.DisposeAsync();
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
+
+
+        ////[HttpPost("GetDataByStudentId/{ID:int}/{ModifyBy:int}")]
+        //[HttpPost("GetDataByStudentId/{ID:int}")]
+        //public async Task<ApiResult<DataTable>> GetDataByStudentId(int ID)
+        //{
+        //    ActionName = "GetDataByStudentId(int ID)";
+        //    return await Task.Run(async () =>
+        //    {
+        //        var result = new ApiResult<DataTable>();
+        //        try
+        //        {
+        //            var mappedData = new EligibleStudentForPlacement
+        //            {
+        //                ID = ID,                    
+        //            };
+        //            result.Data = await _unitOfWork.CompanyMasterRepository.GetDataByStudentId(mappedData);
+        //            await _unitOfWork.SaveChangesAsync();
+
+        //            if (result.Data.Rows.Count > 0)
+        //            {
+        //                result.State = EnumStatus.Success;
+        //                result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+        //            }
+        //            else
+        //            {
+        //                result.State = EnumStatus.Warning;
+        //                result.Message = Constants.MSG_DATA_NOT_FOUND;
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            await _unitOfWork.DisposeAsync();
+        //            // Write error log
+        //            var nex = new NewException
+        //            {
+        //                PageName = PageName,
+        //                ActionName = ActionName,
+        //                Ex = ex,
+        //            };
+        //            await CreateErrorLog(nex, _unitOfWork);
+        //            result.State = EnumStatus.Error;
+        //            result.ErrorMessage = ex.Message;
+        //        }
+        //        return result;
+        //    });
+        //}
+
+
+    }
+
+
+}
+
+
