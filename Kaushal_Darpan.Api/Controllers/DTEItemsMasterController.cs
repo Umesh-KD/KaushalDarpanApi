db84478877be29986fa682b1c2c26c62eec41c5b
vivek.kushwaha <vivek.kushwaha@RPHO05>
2025-10-03 13:11:54 +0530

    #Vivek

--- Kaushal_Darpan.Api/Controllers/DTEItemsMasterController.cs
+++ Kaushal_Darpan.Api/Controllers/DTEItemsMasterController.cs
@@ -1133,6 +1133,7 @@ namespace Kaushal_Darpan.Api.Controllers
             }
             return result;
         }
+       
 
 
         [HttpPost("GetIssueItemList")]
@@ -1143,6 +1144,41 @@ namespace Kaushal_Darpan.Api.Controllers
             try
             {
                 result.Data = await Task.Run(() => _unitOfWork.iDTEItemsMasterRepository.GetIssueItemList(body));
+                result.State = EnumStatus.Success;
+                if (result.Data.Rows.Count == 0)
+                {
+                    result.State = EnumStatus.Success;
+                    result.Message = "No record found.!";
+                    return result;
+                }
+                result.State = EnumStatus.Success;
+                result.Message = "Data load successfully .!";
+            }
+            catch (System.Exception ex)
+            {
+                await _unitOfWork.DisposeAsync();
+                result.State = EnumStatus.Error;
+                result.ErrorMessage = ex.Message;
+                // write error log
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
+
+        [HttpPost("GetInventoryIssueHistoryList")]
+        public async Task<ApiResult<DataTable>> GetInventoryIssueHistoryList([FromBody] InventoryIssueHistoryListModels body)
+        {
+            ActionName = "GetInventoryIssueHistoryList([FromBody] ItemsIssueReturnModels body)";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+                result.Data = await Task.Run(() => _unitOfWork.iDTEItemsMasterRepository.GetInventoryIssueHistoryList(body));
                 result.State = EnumStatus.Success;
                 if (result.Data.Rows.Count == 0)
                 {
