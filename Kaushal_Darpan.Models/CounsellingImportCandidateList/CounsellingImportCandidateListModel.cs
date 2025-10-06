422a6147a87c2f544db16db2db7ae8bdf582824c
Jafar Alam <jafar.alam@corpnet.co.in>
2025-10-06 16:50:05 +0530

    counsellingimportcandidatelist

--- Kaushal_Darpan.Models/CounsellingImportCandidateList/CounsellingImportCandidateListModel.cs
+++ Kaushal_Darpan.Models/CounsellingImportCandidateList/CounsellingImportCandidateListModel.cs
@@ -5,11 +5,11 @@
        public string CandidateName { get; set; }
         public string CandidateFatherName { get; set; }
 
-        public string MobileNo { get; set; }
+        public string? MobileNo { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string SSOID { get; set; }
+        public string? SSOID { get; set; }
 
         //public string IPAddress { get; set; }
 
