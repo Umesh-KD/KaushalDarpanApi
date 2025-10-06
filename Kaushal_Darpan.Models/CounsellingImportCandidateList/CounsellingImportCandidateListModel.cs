955f7caea91910127221c18f86f973bc875d100c
Jafar Alam <jafar.alam@corpnet.co.in>
2025-10-06 12:36:36 +0530

    counsellingimportcandidatelist change

---
+++ Kaushal_Darpan.Models/CounsellingImportCandidateList/CounsellingImportCandidateListModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Kaushal_Darpan.Models.CounsellingImportCandidateListModel
+{
+    public class CounsellingImportExcelModel
+    {
+       public string CandidateName { get; set; }
+        public string CandidateFatherName { get; set; }
+
+        public string IPAddress { get; set; }
+
+    }
+        
+}
