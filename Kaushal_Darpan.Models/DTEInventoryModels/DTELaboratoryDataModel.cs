0a228f29f84b9c10d010aeee7585de06a2d0f43b
divyasharma <divya.sharma@devitpl.com>
2025-10-27 14:39:37 +0530

    #DTE Laboratory Master 27102025

---
+++ Kaushal_Darpan.Models/DTEInventoryModels/DTELaboratoryDataModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaushal_Darpan.Models.DTEInventoryModels
+{
+    public class DTELaboratoryDataModel
+    {
+        public int StreamID { get; set; } = 0;
+        public int LabID { get; set; } = 0;
+        public string LabName { get; set; } = string.Empty;
+        public int staffID { get; set; } = 0;
+        public bool ActiveStatus { get; set; } = true;
+        public bool DeleteStatus { get; set; } = false;
+        public int CreatedBy { get; set; } = 0;
+        public int ModifyBy { get; set; } = 0;
+        public int DepartmentID { get; set; } = 0;
+        public int InstituteID { get; set; } = 0;
+        public int OfficeID { get; set; } = 0;
+        public int RoleID { get; set; } = 0;
+        public string ActionName { get; set; } = string.Empty;
+    }
+}
