7b2ba4ee78969989b008f146c81a9ba094fa1c4b
Raviraj.singh <Raviraj.singh@RPHO139>
2025-10-27 12:16:55 +0530

    #RAVAIK

--- Kaushal_Darpan.Api/Controllers/ITIDataMasterController.cs
+++ Kaushal_Darpan.Api/Controllers/ITIDataMasterController.cs
@@ -145,7 +145,6 @@ namespace Kaushal_Darpan.Api.Controllers
 
         #region ncvt student corrected master api's
 
-
         [HttpPost("GetStudentCorrectionListData")]
         public async Task<ApiResult<DataTable>> GetStudentCorrectionListData([FromBody] StudentCorrectionMasterSearchModel body)
         {
