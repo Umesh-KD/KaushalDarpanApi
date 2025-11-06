c7e782e2383923023cc15ae8452e57804351cefb
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2025-11-06 14:02:47 +0530

    ITI Budget management changes start

---
+++ Kaushal_Darpan.Api/Controllers/ITIBudgetCreateController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Kaushal_Darpan.Core.Helper;
+using Kaushal_Darpan.Core.Interfaces;
+using Kaushal_Darpan.Models.ITI_BGTHeadmaster;
+using Kaushal_Darpan.Models.ITIBUDGET;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Kaushal_Darpan.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ITIBudgetCreateController : BaseController
+    {
+        public override string PageName => "ITIBudgetCreateController";
+        public override string ActionName { get; set; }
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public ITIBudgetCreateController(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpPost("GetAllData")]
+        public async Task<ApiResult<DataTable>> GetAllData(BudgetHeadSearchFilter model)
+        {
+            ActionName = "GetAllData()";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+                result.Data = await _unitOfWork.BudgetHeadManagementRepository.GetAllData(model);
+                if (result.Data.Rows.Count > 0)
+                {
+                    result.State = EnumStatus.Success;
+                    result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+                }
+                else
+                {
+                    result.State = EnumStatus.Warning;
+                    result.Message = Constants.MSG_DATA_NOT_FOUND;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                await _unitOfWork.DisposeAsync();
+                result.State = EnumStatus.Error;
+                result.ErrorMessage = ex.Message;
+                // write error log
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
+
+        [HttpPost("GetITIBudgetDropdown")]
+        public async Task<ApiResult<DataTable>> GetITIBudgetDropdown(ITIBudgetDropdownDataModel model)
+        {
+            ActionName = "GetAllData()";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+                result.Data = await _unitOfWork.ITIBudgetCreateRepository.GetITIBudgetDropdown(model);
+                if (result.Data.Rows.Count > 0)
+                {
+                    result.State = EnumStatus.Success;
+                    result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+                }
+                else
+                {
+                    result.State = EnumStatus.Warning;
+                    result.Message = Constants.MSG_DATA_NOT_FOUND;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                await _unitOfWork.DisposeAsync();
+                result.State = EnumStatus.Error;
+                result.ErrorMessage = ex.Message;
+                // write error log
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
+    }
+}
---
+++ Kaushal_Darpan.Infra/Repositories/ITIBudgetCreateRepository.cs
@@ -0,0 +1,96 @@
+﻿using Kaushal_Darpan.Core.Helper;
+using Kaushal_Darpan.Core.Interfaces;
+using Kaushal_Darpan.Infra.Helper;
+using Kaushal_Darpan.Models.ITI_BGTHeadmaster;
+using Kaushal_Darpan.Models.ITIBUDGET;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaushal_Darpan.Infra.Repositories
+{
+    public class ITIBudgetCreateRepository: I_ITIBudgetCreateRepository
+    {
+        private readonly DBContext _dbContext;
+        private readonly string _pageName;
+        private string _actionName;
+        private string _sqlQuery;
+        private string _IPAddress;
+        public ITIBudgetCreateRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+            _pageName = "ITIBudgetCreateRepository";
+            _IPAddress = CommonFuncationHelper.GetIpAddress();
+        }
+        public async Task<DataTable> GetAllData(BudgetHeadSearchFilter model)
+        {
+            _actionName = "GetAllData()";
+            try
+            {
+                return await Task.Run(async () =>
+                {
+                    DataTable dataTable = new DataTable();
+                    using (var command = await _dbContext.CreateCommandAsync())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "USP_ITI_BGT_CollegeBudgetAlloted_GET";
+                        command.Parameters.AddWithValue("@FinYearID", model.FinYearID);
+                        command.Parameters.AddWithValue("@CollegeID", model.CollegeID);
+                        command.Parameters.AddWithValue("@ActionType", model.ActionName);
+                        _sqlQuery = command.GetSqlExecutableQuery();// Get sql query
+                        dataTable = await command.FillAsync_DataTable();
+                    }
+                    return dataTable;
+                });
+            }
+            catch (Exception ex)
+            {
+                var errorDesc = new ErrorDescription
+                {
+                    Message = ex.Message,
+                    PageName = _pageName,
+                    ActionName = _actionName,
+                    SqlExecutableQuery = _sqlQuery
+                };
+                var errordetails = CommonFuncationHelper.MakeError(errorDesc);
+                throw new Exception(errordetails, ex);
+            }
+        }
+
+        public async Task<DataTable> GetITIBudgetDropdown(ITIBudgetDropdownDataModel model)
+        {
+            _actionName = "GetITIBudgetDropdown(ITIBudgetDropdownDataModel model)";
+            try
+            {
+                return await Task.Run(async () =>
+                {
+                    DataTable dataTable = new DataTable();
+                    using (var command = await _dbContext.CreateCommandAsync())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "USP_ITI_BGT_Dropdowns";
+                        command.Parameters.AddWithValue("@Action", model.Action);
+                        _sqlQuery = command.GetSqlExecutableQuery();// Get sql query
+                        dataTable = await command.FillAsync_DataTable();
+                    }
+                    return dataTable;
+                });
+            }
+            catch (Exception ex)
+            {
+                var errorDesc = new ErrorDescription
+                {
+                    Message = ex.Message,
+                    PageName = _pageName,
+                    ActionName = _actionName,
+                    SqlExecutableQuery = _sqlQuery
+                };
+                var errordetails = CommonFuncationHelper.MakeError(errorDesc);
+                throw new Exception(errordetails, ex);
+            }
+        }
+    }
+}
