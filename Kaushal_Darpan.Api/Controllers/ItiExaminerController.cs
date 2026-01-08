cc9d8475a639760125acc4f63a3c5b19c8cc0972
Raviraj.singh <Raviraj.singh@RPHO139>
2026-01-08 12:59:44 +0530

    #RAVI

--- Kaushal_Darpan.Api/Controllers/ItiExaminerController.cs
+++ Kaushal_Darpan.Api/Controllers/ItiExaminerController.cs
@@ -995,5 +995,44 @@ namespace Kaushal_Darpan.Api.Controllers
             return result;
         }
 
+
+
+
+
+        [HttpPost("CheckExaminerProfileCompleted")]
+        public async Task<ApiResult<DataTable>> CheckExaminerProfileCompleted([FromBody] ItiExaminerSearchModel body)
+        {
+            ActionName = "GetAllData()";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+                result.Data = await Task.Run(() => _unitOfWork.ItiExaminerRepository.CheckExaminerProfileCompleted(body));
+                result.State = EnumStatus.Success;
+                if (result.Data.Rows.Count == 0)
+                {
+                    result.State = EnumStatus.Success;
+                    result.Message = "No record found.!";
+                    return result;
+                }
+                result.State = EnumStatus.Success;
+                result.Message = "Data load successfully .!";
+            }
+            catch (System.Exception ex)
+            {
+                await _unitOfWork.DisposeAsync();
+                result.State = EnumStatus.Error;
+                result.ErrorMessage = ex.Message;
+                // write error log
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
+
     }
 }
