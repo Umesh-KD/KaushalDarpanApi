1a2c9e06baeddf2fd5ab9ef1aaab6e99e415b8af
vivek.kushwaha <vivek.kushwaha@RPHO05>
2026-01-29 17:50:45 +0530

    #vivek

--- Kaushal_Darpan.Models/Annexture/AnnextureModel.cs
+++ Kaushal_Darpan.Models/Annexture/AnnextureModel.cs
@@ -8,11 +8,15 @@ namespace Kaushal_Darpan.Models.LeaveMaster
 {
     public class AnnextureModel
     {
-        public int InstituteID { get; set; } = 0;
-        public int SemesterID { get; set; } = 0;
-        public int Eng_NonEng { get; set; } = 0;
-        public int DepartmentID { get; set; } = 0;
-        public int EndTermID { get; set; } = 0;
+        public int InstituteID { get; set; }
+        public int SemesterID { get; set; }
+        public int Eng_NonEng { get; set; } 
+        public int DepartmentID { get; set; }
+        public int EndTermID { get; set; }
+        public int CourseTypeID { get; set; } 
+        public string? InstituteName { get; set; } 
+        public string? EndTermName { get; set; } 
+        public int? InstituteCode { get; set; } 
 
     }
 }
