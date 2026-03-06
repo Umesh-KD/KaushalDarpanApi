4dcf5ead991ad58da2e449476497167db8555311
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-03-06 15:15:54 +0530

    mass copy report

---
+++ Kaushal_Darpan.Core/Interfaces/IMassCopyReportRepository.cs
@@ -0,0 +1,16 @@
+﻿using Kaushal_Darpan.Models.BTER;
+using Kaushal_Darpan.Models.ITINCVT;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaushal_Darpan.Core.Interfaces
+{
+    public interface IMassCopyReportRepository
+    {
+        Task<DataTable> MassCopyReport_GetListData(MassCopyReportDataModel body);
+    }
+}
---
+++ Kaushal_Darpan.Models/BTER/MassCopyReportDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaushal_Darpan.Models.BTER
+{
+    public class MassCopyReportDataModel
+    {
+        public int Eng_NonEng { get; set; }
+    }
+}
