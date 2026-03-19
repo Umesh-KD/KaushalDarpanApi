8b8ff659d070a3d3e3d0945df5327d5f1e5305e1
vivek.kushwaha <vivek.kushwaha@RPHO05>
2026-03-19 17:37:04 +0530

    #vivek

--- Kaushal_Darpan.Infra/Repositories/ITICollegeMasterRepository .cs
+++ Kaushal_Darpan.Infra/Repositories/ITICollegeMasterRepository .cs
@@ -1286,6 +1286,7 @@ namespace Kaushal_Darpan.Infra.Repositories
                         command.Parameters.AddWithValue("@Remarks", model.Remarks);
                         command.Parameters.AddWithValue("@FinYearId", model.FinYearId);
                         command.Parameters.AddWithValue("@BankID", model.BankID);
+                        command.Parameters.AddWithValue("@Action", model.ActionType);
 
                         _sqlQuery = command.GetSqlExecutableQuery();
                         dataTable = await command.FillAsync_DataTable();
@@ -1448,6 +1449,42 @@ namespace Kaushal_Darpan.Infra.Repositories
                     throw new Exception(errordetails, ex);
                 }
             });
+        }
+
+
+        public async Task<DataTable> statusUpdateById(ITIPlanningStatusUpdateByIdModel body)
+        {
+            _actionName = "statusUpdateById()";
+            try
+            {
+                return await Task.Run(async () =>
+                {
+                    DataTable dataTable = new DataTable();
+                    using (var command = await _dbContext.CreateCommandAsync())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "USP_GetITIPlanningStatusUpdateById";
+                        command.Parameters.AddWithValue("@BankGuaranteeID", body.BankGuaranteeID);
+                        command.Parameters.AddWithValue("@Status", body.Status);
+                        command.Parameters.AddWithValue("@Remarks", body.Remarks);
+                        _sqlQuery = command.GetSqlExecutableQuery();
+                        dataTable = await command.FillAsync_DataTable();
+                    }
+                    return dataTable;
+                });
+            }
+            catch (Exception ex)
+            {
+                var errorDesc = new ErrorDescription
+                {
+                    Message = ex.Message,
+                    PageName = _pageName,
+                    ActionName = _actionName,
+                    SqlExecutableQuery = _sqlQuery
+                };
+                var errordetails = CommonFuncationHelper.MakeError(errorDesc);
+                throw new Exception(errordetails, ex);
+            }
         }
 
 
