6222546bb279445c07d9c457c685813b522f1cd1
vivek.kushwaha <vivek.kushwaha@RPHO05>
2026-04-15 12:04:39 +0530

    changes regarding to insert teacher with semester and branch, get list and history

--- Kaushal_Darpan.Infra/Repositories/CommonFunctionRepository.cs
+++ Kaushal_Darpan.Infra/Repositories/CommonFunctionRepository.cs
@@ -7630,6 +7630,42 @@ namespace Kaushal_Darpan.Infra.Repositories
             }
         }
 
+        public async Task<DataTable> Get_Staff_Ac_Year(StaffMasterDDLDataModel body)
+        {
+            _actionName = "GetAllData()";
+            try
+            {
+                return await Task.Run(async () =>
+                {
+                    DataTable dataTable = new DataTable();
+                    using (var command = await _dbContext.CreateCommandAsync())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "USP_BTER_Get_Staff_Ac_Year";
+                        command.Parameters.AddWithValue("@InstituteID", body.InstituteID);
+                        command.Parameters.AddWithValue("@Eng_NonEng", body.Eng_NonEng);
+                        command.Parameters.AddWithValue("@DepartmentID", body.DepartmentID);
+                        command.Parameters.AddWithValue("@RoleID", body.RoleID);
+                        _sqlQuery = command.GetSqlExecutableQuery();// Get sql query
+                        dataTable = await command.FillAsync_DataTable();
+                    }
+                    return dataTable;
+                });
+            }
+            catch (Exception ex)
+            {
+                var errorDesc = new ErrorDescription
+                {
+                    Message = ex.Message,
+                    PageName = _pageName,
+                    ActionName = _actionName,
+                    SqlExecutableQuery = _sqlQuery
+                };
+                var errordetails = CommonFuncationHelper.MakeError(errorDesc);
+                throw new Exception(errordetails, ex);
+            }
+        }
+
 
         public async Task<DataTable> GetITIStaffInstituteWise(StaffMasterDDLDataModel body)
         {
