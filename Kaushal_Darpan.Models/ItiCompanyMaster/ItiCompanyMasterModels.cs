901e98c8f2204c09921ee64c834c24693c64fde6
Shubh Saini <shubh.saini@corpnet.co.in>
2026-04-17 11:35:57 +0530

    commit

--- Kaushal_Darpan.Models/ItiCompanyMaster/ItiCompanyMasterModels.cs
+++ Kaushal_Darpan.Models/ItiCompanyMaster/ItiCompanyMasterModels.cs
@@ -223,6 +223,7 @@ namespace Kaushal_Darpan.Models.ItiCompanyMaster
         public string? OrderTypeName { get; set; }
         public string? DisOrderDate { get; set; }
         public int OrderType { get; set; }
+        public int TypeID { get; set; }
     }
 
     public class UpdateWorkList
