5b79a2b822f8426019fc0f510341870d6f695f51
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-05-04 15:49:06 +0530

    iti inventory bug fix in update item

--- Kaushal_Darpan.Api/Controllers/ITIInventoryController.cs
+++ Kaushal_Darpan.Api/Controllers/ITIInventoryController.cs
@@ -868,6 +868,11 @@ namespace Kaushal_Darpan.Api.Controllers
                         result.State = EnumStatus.Warning;
                         result.ErrorMessage = Constants.MSG_SAVE_Duplicate;
                     }
+                    else if(result.Data == -3)
+                    {
+                        result.State = EnumStatus.Error;
+                        result.ErrorMessage = "This item has already been approved and cannot be edited.";
+                    }
                     else
                     {
                         result.State = EnumStatus.Error;
