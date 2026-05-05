a4365dc69cf6277a1e06809064d58507e9bc7071
Brijraj Sharma <brijraj.sharma@corpnet.co.in>
2026-05-05 16:52:10 +0530

    add reval parameter for commonsubject ddl master

--- Kaushal_Darpan.Infra/Repositories/CommonFunctionRepository.cs
+++ Kaushal_Darpan.Infra/Repositories/CommonFunctionRepository.cs
@@ -5999,6 +5999,7 @@ namespace Kaushal_Darpan.Infra.Repositories
                     command.Parameters.AddWithValue("@Eng_NonEng", model.Eng_NonEng);
                     command.Parameters.AddWithValue("@DepartmentID", model.DepartmentID);
                     command.Parameters.AddWithValue("@EndTermID", model.EndTermID);
+                    command.Parameters.AddWithValue("@IsReval", model.IsReval);
 
                     _sqlQuery = command.GetSqlExecutableQuery();
                     dataTable = await command.FillAsync_DataTable();
