6323bf1e679508cd20b28ae859b2d8182113c9fb
Brijraj Sharma <brijraj.sharma@corpnet.co.in>
2026-05-11 17:59:01 +0530

    create UI to provide to see already applied and multiple optional subject update functionlity after eligible the students.

--- Kaushal_Darpan.Infra/Repositories/CommonFunctionRepository.cs
+++ Kaushal_Darpan.Infra/Repositories/CommonFunctionRepository.cs
@@ -11992,6 +11992,39 @@ namespace Kaushal_Darpan.Infra.Repositories
                 }
             });
         }
+
+        public async Task<DataTable> GetAlreadyAssignedOptionalSubject(int StudentExamID)
+        {
+            _actionName = "GetAlreadyAssignedOptionalSubject(int StudentExamID)";
+            try
+            {
+                DataTable dataTable = new DataTable();
+                using (var command = await _dbContext.CreateCommandAsync())
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "USP_OptionalSubjectMaster";
+
+                    command.Parameters.AddWithValue("@Action", "_getAlreadyAssignedOptionalSubject");
+                    command.Parameters.AddWithValue("@StudentExamID", StudentExamID);
+
+                    _sqlQuery = command.GetSqlExecutableQuery();
+                    dataTable = await command.FillAsync_DataTable();
+                }
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                var errorDesc = new ErrorDescription
+                {
+                    Message = ex.Message,
+                    PageName = _pageName,
+                    ActionName = _actionName,
+                    SqlExecutableQuery = _sqlQuery
+                };
+                var errordetails = CommonFuncationHelper.MakeError(errorDesc);
+                throw new Exception(errordetails, ex);
+            }
+        }
     }
 }
 
