18ccaebdaf586d9ab2b58c556e863743d0c18744
Shubh Saini <shubh.saini@corpnet.co.in>
2026-05-14 15:29:10 +0530

    COMMIT

--- Kaushal_Darpan.Models/DTEInventoryModels/DTEEquipmentsModel.cs
+++ Kaushal_Darpan.Models/DTEInventoryModels/DTEEquipmentsModel.cs
@@ -11,7 +11,7 @@ namespace Kaushal_Darpan.Models.DTEInventoryModels
         public int EquipmentsId { get; set; }
         public string Name { get; set; }
         public int UnitId { get; set; }
-        public string Specification { get; set; }
+        public string? Specification { get; set; }
         public bool ActiveStatus { get; set; }
         public bool DeleteStatus { get; set; }
         public DateTime? RTS { get; set; }
