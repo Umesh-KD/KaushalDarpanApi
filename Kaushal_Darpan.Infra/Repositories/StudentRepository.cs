980ecff701a4506ad2ce8937a6b3e0cdb851f645
Umesh Rajpoot <umesh.rajpoot@corpnet.co.in>
2026-05-20 13:53:19 +0530

    GetRosterDisplay_PDFTimeTable

--- Kaushal_Darpan.Infra/Repositories/StudentRepository.cs
+++ Kaushal_Darpan.Infra/Repositories/StudentRepository.cs
@@ -2135,6 +2135,7 @@ namespace Kaushal_Darpan.Infra.Repositories
                         command.Parameters.AddWithValue("@StreamID", model.StreamID);
                         command.Parameters.AddWithValue("@SubjectID", model.SubjectID);
                         command.Parameters.AddWithValue("@StaffID", model.StaffID);
+                        command.Parameters.AddWithValue("@InstituteId", model.InstituteId);
                         _sqlQuery = command.GetSqlExecutableQuery();
                         ds = await command.FillAsync();
                     }
