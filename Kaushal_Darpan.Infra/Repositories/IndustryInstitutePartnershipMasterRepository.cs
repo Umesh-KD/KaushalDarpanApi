e2d57afdf2fd2048ca27a671cfadb73aa2261c2e
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-05-28 12:15:40 +0530

    iip consent report

--- Kaushal_Darpan.Infra/Repositories/IndustryInstitutePartnershipMasterRepository.cs
+++ Kaushal_Darpan.Infra/Repositories/IndustryInstitutePartnershipMasterRepository.cs
@@ -993,5 +993,41 @@ namespace Kaushal_Darpan.Infra.Repositories
                 throw new Exception(errordetails, ex);
             }
         }
+
+        public async Task<DataTable> GetIIPEventConsentReportData(CompanyEventSearchModel body)
+        {
+            _actionName = "GetIIPEventConsentReportData(CompanyEventSearchModel body)";
+            try
+            {
+                DataTable dataTable = new DataTable();
+                using (var command = await _dbContext.CreateCommandAsync())
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "USP_IIP_GetConsentData";
+                    command.Parameters.AddWithValue("@Action", body.Action);
+
+                    command.Parameters.AddWithValue("@EventID", body.EventID);
+                    command.Parameters.AddWithValue("@RoleID", body.RoleID);
+                    command.Parameters.AddWithValue("@UserID", body.UserID);
+
+                    _sqlQuery = command.GetSqlExecutableQuery();
+                    dataTable = await command.FillAsync_DataTable();
+                }
+
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                var errorDesc = new ErrorDescription
+                {
+                    Message = ex.Message,
+                    PageName = _pageName,
+                    ActionName = _actionName,
+                    SqlExecutableQuery = _sqlQuery
+                };
+                var errordetails = CommonFuncationHelper.MakeError(errorDesc);
+                throw new Exception(errordetails, ex);
+            }
+        }
     }
 }
