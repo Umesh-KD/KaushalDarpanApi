2b207b84acf0415a7f39cc800b2599e7256d1e66
Umesh Rajpoot <umesh.rajpoot@corpnet.co.in>
2026-06-05 10:22:32 +0530

    APIforStatePortal code commit

---
+++ Kaushal_Darpan.Infra/Repositories/APIforStatePortalRepository.cs
@@ -0,0 +1,66 @@
+﻿using Kaushal_Darpan.Core.Helper;
+using Kaushal_Darpan.Core.Interfaces;
+using Kaushal_Darpan.Infra.Helper;
+using Kaushal_Darpan.Models;
+using Kaushal_Darpan.Models.StudentApplyForHostel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaushal_Darpan.Infra.Repositories
+{
+    public class APIforStatePortalRepository: IAPIforStatePortalRepository
+    {
+        private readonly DBContext _dbContext;
+        private readonly string _pageName;
+        private string _actionName;
+        private string _sqlQuery;
+        private string _IPAddress;
+
+        public APIforStatePortalRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+            _pageName = "APIforStatePortalRepository";
+            _IPAddress = CommonFuncationHelper.GetIpAddress();
+        }
+
+        public async Task<DataTable> GetAPIforStatePortal(APIforStatePortalModel SearchReq)
+        {
+            _actionName = "APIforStatePortal()";
+            return await Task.Run(async () =>
+            {
+                try
+                {
+                    DataTable dataTable = new DataTable();
+                    using (var command = await _dbContext.CreateCommandAsync())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "USP_APIforStatePortal";
+                        command.Parameters.AddWithValue("@DepartmentID", SearchReq.DepartmentID);
+                        command.Parameters.AddWithValue("@FinancialYearID", SearchReq.FinancialYearID);
+                       
+                        _sqlQuery = command.GetSqlExecutableQuery();
+                        dataTable = await command.FillAsync_DataTable();
+                    }
+                    return dataTable;
+                }
+                catch (Exception ex)
+                {
+                    var errorDesc = new ErrorDescription
+                    {
+                        Message = ex.Message,
+                        PageName = _pageName,
+                        ActionName = _actionName,
+                        SqlExecutableQuery = _sqlQuery
+                    };
+                    var errordetails = CommonFuncationHelper.MakeError(errorDesc);
+                    throw new Exception(errordetails, ex);
+                }
+            });
+        }
+
+    }
+}
---
+++ Kaushal_Darpan.Models/APIforStatePortalModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaushal_Darpan.Models
+{
+    public class APIforStatePortalModel
+    {
+        public int? DepartmentID { get; set; }
+
+        public int? FinancialYearID { get; set; }
+
+    }
+}
