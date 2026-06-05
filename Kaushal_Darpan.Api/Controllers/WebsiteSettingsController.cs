c8574c453c0368bbfbe0fd75b65b93bea215b4a0
Shubh Saini <shubh.saini@corpnet.co.in>
2026-06-05 17:33:57 +0530

    wq

--- Kaushal_Darpan.Api/Controllers/WebsiteSettingsController.cs
+++ Kaushal_Darpan.Api/Controllers/WebsiteSettingsController.cs
@@ -388,6 +388,40 @@ namespace Kaushal_Darpan.Api.Controllers
             }
             return result;
         }
-
+        [HttpPost("GetAllDataOrders")]
+        public async Task<ApiResult<DataTable>> GetAllDataOrders(WebsiteSettingDataModel request)
+        {
+            ActionName = "GetAllData(DateConfigurationModel request)";
+            var result = new ApiResult<DataTable>();
+            try
+            {
+                result.Data = await _unitOfWork.WebsiteSettingsRepository.GetAllDataOrders(request);
+                if (result.Data.Rows.Count > 0)
+                {
+                    result.State = EnumStatus.Success;
+                    result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
+                }
+                else
+                {
+                    result.State = EnumStatus.Warning;
+                    result.Message = Constants.MSG_DATA_NOT_FOUND;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                await _unitOfWork.DisposeAsync();
+                result.State = EnumStatus.Error;
+                result.ErrorMessage = ex.Message;
+                // write error log
+                var nex = new NewException
+                {
+                    PageName = PageName,
+                    ActionName = ActionName,
+                    Ex = ex,
+                };
+                await CreateErrorLog(nex, _unitOfWork);
+            }
+            return result;
+        }
     }
 }
