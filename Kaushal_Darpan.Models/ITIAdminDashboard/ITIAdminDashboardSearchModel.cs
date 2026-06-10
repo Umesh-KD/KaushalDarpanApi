f94efb67bc2695cd49b9007743fde43ecfa4f6a1
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-06-10 16:36:01 +0530

    establishment dashboard

--- Kaushal_Darpan.Models/ITIAdminDashboard/ITIAdminDashboardSearchModel.cs
+++ Kaushal_Darpan.Models/ITIAdminDashboard/ITIAdminDashboardSearchModel.cs
@@ -74,10 +74,13 @@ namespace Kaushal_Darpan.Models.ITIAdminDashboard
 
     public class ITIEstablishmentDashboardSearchModel
     {
-        public int DepartmentID { get; set; } = 2;
-        public int RoleID { get; set; } = 0;
-        public int EndTermID { get; set; } = 0;
-        public int Eng_NonEng { get; set; } = 0;
+        public int? DepartmentID { get; set; } = 2;
+        public int? RoleID { get; set; } = 0;
+        public int? EndTermID { get; set; } = 0;
+        public int? Eng_NonEng { get; set; } = 0;
+        public int? InstituteID { get; set; } = 0;
+        public int? UserID { get; set; } = 0;
+        public int? OfficeID { get; set; } = 0;
     }
 
     public class PostPlanningDashboardDataModel
