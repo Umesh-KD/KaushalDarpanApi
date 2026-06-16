7f4b6139a54481295af156ccec451f2c735e98a5
Shubh Saini <shubh.saini@corpnet.co.in>
2026-06-16 16:32:35 +0530

    ew

--- Kaushal_Darpan.Models/CenterCreationMaster/CenterCreationModel.cs
+++ Kaushal_Darpan.Models/CenterCreationMaster/CenterCreationModel.cs
@@ -26,16 +26,16 @@
     {
         public int CenterID { get; set; }
         public int Id { get; set; }
-        public string InstituteCode { get; set; }
+        public string? InstituteCode { get; set; }
         public string? AdmissionCategory { get; set; }
         public string? MobileNumber { get; set; }
         public int DistrictID { get; set; }
-        public int DivisionID { get; set; }
-        public int TehsilID { get; set; }
-        public string Institutename { get; set; }
+        public int? DivisionID { get; set; }
+        public int? TehsilID { get; set; }
+        public string? Institutename { get; set; }
         public string? Address { get; set; }
         public string? PinCode { get; set; }
-        public string SSOID { get; set; }
+        public string? SSOID { get; set; }
         public int ModifyBy { get; set; }
         public string? IPAddress { get; set; }
         public int? CCCode { get; set; }
