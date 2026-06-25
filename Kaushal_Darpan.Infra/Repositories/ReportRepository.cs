16872f6b4a897f4474b592770d8050a562c3b4a1
Raviraj.singh <Raviraj.singh@RPHO139>
2026-06-25 15:20:31 +0530

    #RAVI

--- Kaushal_Darpan.Infra/Repositories/ReportRepository.cs
+++ Kaushal_Darpan.Infra/Repositories/ReportRepository.cs
@@ -9406,6 +9406,23 @@ namespace Kaushal_Darpan.Infra.Repositories
                         command.Parameters.AddWithValue("@PresentStatus", model.PresentStatus);
                         command.Parameters.AddWithValue("@Type", model.Type);
                     }
+
+                    else if (model.Type == 8)
+                    {
+                        command.CommandText = "USP_90AboveSessionalMarksInstituteWiseSemester";
+
+                        //command.Parameters.AddWithValue("@InstituteID", model.InstituteID);
+                        //command.Parameters.AddWithValue("@EndTermID", model.EndTermID);
+                        //command.Parameters.AddWithValue("@CourseTypeID", model.CourseType);
+                        //command.Parameters.AddWithValue("@Action", model.Action);
+                        command.Parameters.AddWithValue("@InstituteID", 1);
+                        command.Parameters.AddWithValue("@EndTermID", model.EndTermID);
+                        command.Parameters.AddWithValue("@CourseTypeID", model.Eng_NonEng);
+                        command.Parameters.AddWithValue("@Action", model.Action);
+                    }
+
+
+
                     else
                     {
                         throw new Exception("Invalide request!");
