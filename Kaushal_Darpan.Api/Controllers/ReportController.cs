d81315ec99dc2e5d44b0e8c65efb55d77deef3f6
Umesh Rajpoot <umesh.rajpoot@corpnet.co.in>
2026-06-30 12:25:51 +0530

    Miscellaneous-Report code commit

--- Kaushal_Darpan.Api/Controllers/ReportController.cs
+++ Kaushal_Darpan.Api/Controllers/ReportController.cs
@@ -16695,10 +16695,10 @@ namespace Kaushal_Darpan.Api.Controllers
             try
             {
                 // Pass the entire model to the repository
-                //result.Data = await _unitOfWork.ReportRepository.GetMiscellaneousReport(model);
-                var action = "_get_data_to_test";
-                var ds = await _unitOfWork.CommonFunctionRepository.Dummy_GetTestUspDataByAction(action);
-                result.Data = ds.Tables[0];
+                result.Data = await _unitOfWork.ReportRepository.GetMiscellaneousReport(model);
+                //var action = "_get_data_to_test";
+                //var ds = await _unitOfWork.CommonFunctionRepository.Dummy_GetTestUspDataByAction(action);
+                //result.Data = ds.Tables[0];
                 if (result.Data.Rows.Count > 0)
                 {
                     result.State = EnumStatus.Success;
