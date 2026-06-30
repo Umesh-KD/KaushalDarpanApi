d855d2f3fe5940aa97120920317d95f25a222c97
Shubh Saini <shubh.saini@corpnet.co.in>
2026-06-30 16:25:46 +0530

    RE

--- Kaushal_Darpan.Infra/Repositories/ITICollegeMasterRepository .cs
+++ Kaushal_Darpan.Infra/Repositories/ITICollegeMasterRepository .cs
@@ -1686,16 +1686,62 @@ namespace Kaushal_Darpan.Infra.Repositories
         }
 
 
+        //public async Task<bool> UpdateCampusStatusByID(ITICampusStatusModel request)
+        //{
+        //    _actionName = "UpdateCampusStatusByID(ITICampusStatusModel request)";
+        //    try
+        //    {
+        //        int result = 0;
+        //        using (var command = await _dbContext.CreateCommandAsync(true))
+        //        {
+        //            command.CommandText = "USP_UpdateIndependentCollegeCreatedStatus";
+        //            command.CommandType = CommandType.StoredProcedure;
+        //            command.Parameters.AddWithValue("@Id", request.Id);
+        //            command.Parameters.AddWithValue("@ModifyBy", request.ModifyBy);
+        //            command.Parameters.AddWithValue("@CampusRemovedRemark", request.CampusRemovedRemark);
+        //            command.Parameters.AddWithValue("@IPAddress", _IPAddress);
+        //            command.Parameters.AddWithValue("@CampusRemovedOrderNo", request.CampusRemovedOrderNo);
+        //            command.Parameters.AddWithValue("@CampusRemovedOrderDate", request.CampusRemovedOrderDate);
+        //            command.Parameters.AddWithValue("@CampusRemovedFilePath", request.CampusRemovedFilePath);
+        //            command.Parameters.AddWithValue("@CampusRemovedDisFilePath", request.CampusRemovedDisFilePath);
+
+
+        //            var returnParam = new SqlParameter("@Return", SqlDbType.Int) { Direction = ParameterDirection.Output };
+        //            command.Parameters.Add(returnParam);
+
+
+        //            _sqlQuery = command.GetSqlExecutableQuery();
+        //            result = await command.ExecuteNonQueryAsync();
+        //        }
+        //        if (result > 0)
+        //            return true;
+        //        else
+        //            return false;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        var errorDesc = new ErrorDescription
+        //        {
+        //            Message = ex.Message,
+        //            PageName = _pageName,
+        //            ActionName = _actionName,
+        //            SqlExecutableQuery = _sqlQuery
+        //        };
+        //        var errorDetails = CommonFuncationHelper.MakeError(errorDesc);
+        //        throw new Exception(errorDetails, ex);
+        //    }
+        //}
         public async Task<bool> UpdateCampusStatusByID(ITICampusStatusModel request)
         {
             _actionName = "UpdateCampusStatusByID(ITICampusStatusModel request)";
+
             try
             {
-                int result = 0;
                 using (var command = await _dbContext.CreateCommandAsync(true))
                 {
                     command.CommandText = "USP_UpdateIndependentCollegeCreatedStatus";
                     command.CommandType = CommandType.StoredProcedure;
+
                     command.Parameters.AddWithValue("@Id", request.Id);
                     command.Parameters.AddWithValue("@ModifyBy", request.ModifyBy);
                     command.Parameters.AddWithValue("@CampusRemovedRemark", request.CampusRemovedRemark);
@@ -1705,15 +1751,20 @@ namespace Kaushal_Darpan.Infra.Repositories
                     command.Parameters.AddWithValue("@CampusRemovedFilePath", request.CampusRemovedFilePath);
                     command.Parameters.AddWithValue("@CampusRemovedDisFilePath", request.CampusRemovedDisFilePath);
 
-
+                    SqlParameter returnParam = new SqlParameter("@Return", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(returnParam);
 
                     _sqlQuery = command.GetSqlExecutableQuery();
-                    result = await command.ExecuteNonQueryAsync();
+
+                    await command.ExecuteNonQueryAsync();
+
+                    int result = Convert.ToInt32(returnParam.Value);
+
+                    return result > 0;
                 }
-                if (result > 0)
-                    return true;
-                else
-                    return false;
             }
             catch (Exception ex)
             {
@@ -1724,11 +1775,11 @@ namespace Kaushal_Darpan.Infra.Repositories
                     ActionName = _actionName,
                     SqlExecutableQuery = _sqlQuery
                 };
+
                 var errorDetails = CommonFuncationHelper.MakeError(errorDesc);
                 throw new Exception(errorDetails, ex);
             }
         }
-
         public async Task<List<BankGuaranteeConsolidatedReportModel>>
     GetBankGuaranteeConsolidatedReport(BankGuaranteeConsolidatedReportRequest request)
         {
