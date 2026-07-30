3dec68b9771ac5e163e490af3d5150a2eb5f4760
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-07-30 10:45:12 +0530

    bter establishment pay level master

---
+++ Kaushal_Darpan.Core/Interfaces/IPayLevelMasterRepository.cs
@@ -0,0 +1,17 @@
+﻿using Kaushal_Darpan.Models.BTER_EstablishManagement;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaushal_Darpan.Core.Interfaces
+{
+    public interface IPayLevelMasterRepository
+    {
+        Task<int> SavePayLevelMasterData(PayLevelMasterDataModel request);
+        Task<DataTable> GetPayLevelMasterData(PayLevelMasterDataModel body);
+        Task<bool> DeletePayLevel_ByID(PayLevelMasterDataModel request);
+    }
+}
