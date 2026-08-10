e4c92b8fd62f9df48524a07946bdcc837a79ac9c
Shubh Saini <shubh.saini@corpnet.co.in>
2026-08-10 15:05:21 +0530

    commit

--- Kaushal_Darpan.Infra/Repositories/UpwardMovementRepository.cs
+++ Kaushal_Darpan.Infra/Repositories/UpwardMovementRepository.cs
@@ -106,28 +106,33 @@ namespace Kaushal_Darpan.Infra.Repositories
         public async Task<bool> ITIUpwardMomentUpdate(UpwardMoment model)
         {
             _actionName = "ITIUpwardMomentUpdate(UpwardMoment model)";
+
             try
             {
-                int result = 0;
+                int retval = 0;
+
                 using (var command = await _dbContext.CreateCommandAsync(true))
                 {
-                    command.CommandType = CommandType.Text;
-                    command.CommandText = "USP_ITI_GetApplicationStatus";
-
-                    // Add parameters to the stored procedure from the model
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "USP_UpdateStudentSeatAllotmentUpward";
 
                     command.Parameters.AddWithValue("@IsUpward", model.IsUpward);
                     command.Parameters.AddWithValue("@ModifyBy", model.UserID);
                     command.Parameters.AddWithValue("@AllotmentId", model.AllotmentId);
-                   
+
+                    var retValParameter = command.Parameters.Add("@Retval", SqlDbType.Int);
+                    retValParameter.Direction = ParameterDirection.Output;
 
                     _sqlQuery = command.GetSqlExecutableQuery();
-                    result = await command.ExecuteNonQueryAsync();
+
+                    await command.ExecuteNonQueryAsync();
+
+                    retval = Convert.ToInt32(
+                        command.Parameters["@Retval"].Value
+                    );
                 }
-                if (result > 0)
-                    return true;
-                else
-                    return false;
+
+                return retval > 0;
             }
             catch (Exception ex)
             {
@@ -138,7 +143,9 @@ namespace Kaushal_Darpan.Infra.Repositories
                     ActionName = _actionName,
                     SqlExecutableQuery = _sqlQuery
                 };
+
                 var errorDetails = CommonFuncationHelper.MakeError(errorDesc);
+
                 throw new Exception(errorDetails, ex);
             }
         }
