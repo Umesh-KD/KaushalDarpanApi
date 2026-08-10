22a578a78bece2f90d343f2ae7273b3f60ba83fe
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-08-10 13:01:00 +0530

    grievance module bug fix and changes

--- Kaushal_Darpan.Api/Controllers/CommonFunctionController.cs
+++ Kaushal_Darpan.Api/Controllers/CommonFunctionController.cs
@@ -6184,81 +6184,43 @@ namespace Kaushal_Darpan.Api.Controllers
         }
 
         [HttpGet("GetSubjectForCitizenSugg/{selectedOption}")]
-
         public async Task<ApiResult<List<CommonDDLModel>>> GetSubjectForCitizenSugg(int selectedOption)
-
         {
-
-            ActionName = "ITIPlacementCompanyMaster()";
-
+            ActionName = "GetSubjectForCitizenSugg(int selectedOption)";
             return await Task.Run(async () =>
-
             {
-
                 var result = new ApiResult<List<CommonDDLModel>>();
-
                 try
-
                 {
-
                     var data = await _unitOfWork.CommonFunctionRepository.GetSubjectForCitizenSugg(selectedOption);
-
                     if (data != null)
-
                     {
-
                         result.Data = data;
-
                         result.State = EnumStatus.Success;
-
                         result.Message = Constants.MSG_DATA_LOAD_SUCCESS;
-
                     }
-
                     else
-
                     {
-
                         result.State = EnumStatus.Warning;
-
                         result.Message = Constants.MSG_DATA_NOT_FOUND;
-
                     }
-
                 }
-
                 catch (Exception ex)
-
                 {
-
                     await _unitOfWork.DisposeAsync();
-
                     result.State = EnumStatus.Error;
-
                     result.ErrorMessage = ex.Message;
-
                     // write error log
-
                     var nex = new NewException
-
                     {
-
                         PageName = PageName,
-
                         ActionName = ActionName,
-
                         Ex = ex,
-
                     };
-
                     await CreateErrorLog(nex, _unitOfWork);
-
                 }
-
                 return result;
-
             });
-
         }
 
         [HttpGet("GetManageHostelWardenRole/{SSOID}/{RoleID=0}")]
