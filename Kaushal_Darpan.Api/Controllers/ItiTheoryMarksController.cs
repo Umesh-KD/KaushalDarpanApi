6a13ea124811e898424935e670b50428a6f8de96
Shubh Saini <shubh.saini@corpnet.co.in>
2026-09-02 14:55:57 +0530

    commi

--- Kaushal_Darpan.Api/Controllers/ItiTheoryMarksController.cs
+++ Kaushal_Darpan.Api/Controllers/ItiTheoryMarksController.cs
@@ -303,6 +303,52 @@ namespace Kaushal_Darpan.Api.Controllers
         }
 
 
+        [HttpPost("UnlockBundle1")]
+        public async Task<ApiResult<bool>> UnlockBundle1([FromBody] RevertBundleModel request)
+        {
+            ActionName = "UpdateSaveData([FromBody] RevertBundleModel request)";
+            return await Task.Run(async () =>
+            {
+                var result = new ApiResult<bool>();
+                try
+                {
+
+                    // Pass the list to the repository for batch update
+                    var isSave = await _unitOfWork.ItiTheoryMarksRepository.UnlockBundle1(request);
+                    await _unitOfWork.SaveChangesAsync();  // Commit changes if everything is successful
+
+                    if (isSave > 0)
+                    {
+                        result.Data = true;
+                        result.State = EnumStatus.Success;
+                        result.Message = Constants.MSG_UPDATE_SUCCESS;
+                    }
+                    else
+                    {
+                        result.State = EnumStatus.Error;
+                        result.ErrorMessage = Constants.MSG_UPDATE_ERROR;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await _unitOfWork.DisposeAsync();
+                    result.State = EnumStatus.Error;
+                    result.ErrorMessage = ex.Message;
+
+                    // Log the error
+                    var nex = new NewException
+                    {
+                        PageName = PageName,
+                        ActionName = ActionName,
+                        Ex = ex,
+                    };
+                    await CreateErrorLog(nex, _unitOfWork);
+                }
+                return result;
+            });
+        }
+
+
 
         [HttpPost("GetTraineeStudent")]
         public async Task<ApiResult<DataTable>> GetTraineeStudent([FromBody] TheorySearchModel body)
